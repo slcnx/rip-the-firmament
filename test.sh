aa8f6e34a3ff2d563de96b4e52694b37e1f1e5bb
root <root@localhost.localdomain>
2018-01-27 16:42:04 +0800

    fish

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ systemctl start libvirtd.service
 # create NAT bridge
 brctl addbr br-int
 iptables -t nat -A POSTROUTING -j MASQUERADE
+ifconfig br-int 172.17.0.1 netmask 255.255.0.0 up
 sysctl -w net.ipv4.ip_forward=1
-ip addr add 172.16.0.0/16 dev br-int
