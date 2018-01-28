896d4c1e9245dc8bacd9688443d8bff0397a7b74
slcnx <1062670898@qq.com>
2018-01-28 12:46:50 +0800

    ruige-v1

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ systemctl start libvirtd.service
 
 # create NAT bridge
 brctl addbr br-int
+iptables -t nat -F POSTROUTING
 iptables -t nat -A POSTROUTING -j MASQUERADE
 sysctl -w net.ipv4.ip_forward=1
-ip addr add 172.16.0.0/16 dev br-int
+ifconfig br-int 172.16.0.1/16 up
